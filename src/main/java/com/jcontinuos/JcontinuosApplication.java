72a58af056ccd59644d8c77d00d0673180713ac6
LeonardoAndriotti <leonardoandriotte@hotmail.com>
2018-07-19 22:14:56 -0300

    primeiro commit

---
+++ src/main/java/com/jcontinuos/JcontinuosApplication.java
@@ -0,0 +1,12 @@
+package com.jcontinuos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcontinuosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcontinuosApplication.class, args);
+	}
+}
